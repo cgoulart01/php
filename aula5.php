2d266deb3e4bd848b28487aa561f2dbd8ee879cf
cgoulart01 <cgoulart.rj@gmail.com>
2021-06-29 23:44:18 -0300

    estruturas condicionais

---
+++ aula5.php
@@ -0,0 +1,23 @@
+<?php
+/** estruturas condicionais:
+ * igual (==)
+ * maior (>)
+ * menor (<)
+ * maior ou igual (>=)
+ * menor ou igual (<=)
+ * uma ou outra condição é valida ( || ou or )
+ * as duas condições tem que ser validas (&& ou and) 
+ */
+$idade = 18;
+
+echo "Olá mundo!" . PHP_EOL;
+echo "Eu tenho \"$idade\" anos";
+
+echo PHP_EOL . PHP_EOL;
+
+echo "Você só pode entrar, se tiver a partir de 18 anos\n";
+if ($idade>=18){
+echo "Você tem $idade anos. Pode entrar";
+}else{
+echo "Voce tem $idade anos. Você não pode entrar no site.";
+}
